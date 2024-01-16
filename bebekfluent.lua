dfbaddf8076cb348751bb85de3c34ebb6aa0870b
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2024-01-16 11:11:16 +0700

    Update bebekfluent.lua

--- bebekfluent.lua
+++ bebekfluent.lua
@@ -20,65 +20,20 @@ local Tabs = {
 
 local Options = Fluent.Options
 
---cet
-local teleportEnabled = false
-local teleportTimer = 48
---end cet
-
---tp
-local function TeleportToDestination(destination)
-    -- Your teleport function here
-    local Players = game:GetService("Players")
-    local LocalPlayer = Players.LocalPlayer
-    local Character = LocalPlayer.Character
-    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
-
-    local SeatPart = Humanoid.SeatPart
-    local Vehicle = SeatPart.Parent
-
-    if SeatPart:FindFirstAncestor("Body") then
-        Vehicle = SeatPart:FindFirstAncestor("Body").Parent
-    end
-
-    Vehicle.PrimaryPart = SeatPart
-
-    Character.Parent = Vehicle
-    Vehicle:MoveTo(destination)
-
-    -- Menunggu sampai vehicle mencapai tujuan
-    Vehicle.PrimaryPart.Anchored = true
-    wait(2) -- Menunggu selama 2 detik (sesuaikan dengan durasi yang diinginkan)
-    Vehicle.PrimaryPart.Anchored = false
-
-    Character.Parent = workspace
-end
-
-local function GetWaypointDestination(waypoint)
-    local destinations = {
-        ["Silambat Palimanan"] = Vector3.new(-13001.578, 1058.455, -16360.201),
-        ["Rest Area Km 279"] = Vector3.new(-25910.939, 1059.604, -43896.273),
-        ["SuperMart Pekalongan"] = Vector3.new(-38720.934, 1018.218, -62491.059),
-        ["PT.CDID Cargo Cirebon"] = Vector3.new(-21803.887, 1046.989, -27817.059),
-        ["Rojod Semarang"] = Vector3.new(-50889.66, 1017.867, -86514.797)
-    }
-
-    return destinations[waypoint]
-end
---end tp
-
 do
     Fluent:Notify({
         Title = "Aktif Bos",
-        Content = "BUBUUUBUBUBU",
+        Content = "Udah Aktif Ya Bos",
+        SubContent = "SubContent", -- Optional
         Duration = 5 -- Set to nil to make the notification not disappear
     })
 
 
 
-    -- Tabs.Main:AddParagraph({
-    --     Title = "Paragraph",
-    --     Content = "This is a paragraph.\nSecond line!"
-    -- })
+    Tabs.Main:AddParagraph({
+        Title = "Paragraph",
+        Content = "This is a paragraph.\nSecond line!"
+    })
 
 
 
@@ -108,41 +63,35 @@ do
     })
 
 
-    --toggle auto job truck
-    local Toggle = Tabs.Main:AddToggle("Auto Job Truck", {Title = "Toggle", Default = false })
+
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
 
     Toggle:OnChanged(function()
-        --print("Auto Job Truck Start:", Options.MyToggle.Value)
-        Fluent:Notify({
-            Title = "Notification",
-            Content = "Auto Job Truck Start",
-            SubContent = "SubContent", -- Optional
-            Duration = 3 -- Set to nil to make the notification not disappear
-        })
+        print("Toggle changed:", Options.MyToggle.Value)
     end)
 
     Options.MyToggle:SetValue(false)
-    -- end toggle auto job truck
 
-    --Speed
+
+    
     local Slider = Tabs.Main:AddSlider("Slider", {
         Title = "Slider",
         Description = "This is a slider",
-        Default = 20,
-        Min = 20,
-        Max = 500,
+        Default = 2,
+        Min = 0,
+        Max = 5,
         Rounding = 1,
         Callback = function(Value)
-            game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+            print("Slider was changed:", Value)
         end
     })
 
     Slider:OnChanged(function(Value)
-        --print("Slider changed:", Value)
+        print("Slider changed:", Value)
     end)
 
     Slider:SetValue(3)
-    --END speed
+
 
 
     local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
@@ -152,13 +101,108 @@ do
         Default = 1,
     })
 
-    Dropdown:SetValue("one")
+    Dropdown:SetValue("four")
 
     Dropdown:OnChanged(function(Value)
         print("Dropdown changed:", Value)
     end)
 
 
+    
+    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
+        Title = "Dropdown",
+        Description = "You can select multiple values.",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = true,
+        Default = {"seven", "twelve"},
+    })
+
+    MultiDropdown:SetValue({
+        three = true,
+        five = true,
+        seven = false
+    })
+
+    MultiDropdown:OnChanged(function(Value)
+        local Values = {}
+        for Value, State in next, Value do
+            table.insert(Values, Value)
+        end
+        print("Mutlidropdown changed:", table.concat(Values, ", "))
+    end)
+
+
+
+    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
+        Title = "Colorpicker",
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    Colorpicker:OnChanged(function()
+        print("Colorpicker changed:", Colorpicker.Value)
+    end)
+    
+    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+
+
+
+    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
+        Title = "Colorpicker",
+        Description = "but you can change the transparency.",
+        Transparency = 0,
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    TColorpicker:OnChanged(function()
+        print(
+            "TColorpicker changed:", TColorpicker.Value,
+            "Transparency:", TColorpicker.Transparency
+        )
+    end)
+
+
+
+    local Keybind = Tabs.Main:AddKeybind("Keybind", {
+        Title = "KeyBind",
+        Mode = "Toggle", -- Always, Toggle, Hold
+        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+        -- Occurs when the keybind is clicked, Value is `true`/`false`
+        Callback = function(Value)
+            print("Keybind clicked!", Value)
+        end,
+
+        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+        ChangedCallback = function(New)
+            print("Keybind changed!", New)
+        end
+    })
+
+    -- OnClick is only fired when you press the keybind and the mode is Toggle
+    -- Otherwise, you will have to use Keybind:GetState()
+    Keybind:OnClick(function()
+        print("Keybind clicked:", Keybind:GetState())
+    end)
+
+    Keybind:OnChanged(function()
+        print("Keybind changed:", Keybind.Value)
+    end)
+
+    task.spawn(function()
+        while true do
+            wait(1)
+
+            -- example for checking if a keybind is being pressed
+            local state = Keybind:GetState()
+            if state then
+                print("Keybind is being held down")
+            end
+
+            if Fluent.Unloaded then break end
+        end
+    end)
+
+    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
 
 
     local Input = Tabs.Main:AddInput("Input", {
