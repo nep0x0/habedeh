1e1c31fc404658fe83f3cba5002c02fd501f9186
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2024-01-16 11:35:53 +0700

    Update bebekfluent.lua

--- bebekfluent.lua
+++ bebekfluent.lua
@@ -3,96 +3,200 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Bebek Hub v1.2.1 Beta 9 dev 2",
+    Title = "Fluent " .. Fluent.Version,
+    SubTitle = "by dawid",
+    TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
-    Acrylic = true,
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
     Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
-local Main = Window:AddTab({ Title = "Main", Icon = "" })
-local Other = Window:AddTab({ Title = "Other", Icon = "settings" })
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
 
-Main:AddSlider("Walkspeed", {
-    Title = "Walkspeed",
-    Default = 16,
-    Min = 16,
-    Max = 500,
-    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+local Options = Fluent.Options
+
+local function TeleportToDestination(destination)
+    -- Your teleport function here
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local Character = LocalPlayer.Character
+    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+
+    local SeatPart = Humanoid.SeatPart
+    local Vehicle = SeatPart.Parent
+
+    if SeatPart:FindFirstAncestor("Body") then
+        Vehicle = SeatPart:FindFirstAncestor("Body").Parent
     end
-})
 
-Main:AddSlider("Jumppower", {
-    Title = "Jumppower",
-    Default = 50,
-    Min = 50,
-    Max = 350,
-    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-    end
-})
+    Vehicle.PrimaryPart = SeatPart
 
--- ... (Sisanya kode Anda diubah ke Fluent UI)
--- ... (Bagian kode sebelumnya)
+    Character.Parent = Vehicle
+    Vehicle:MoveTo(destination)
 
--- Teleportasi
-Other:AddToggle("Job Truk", {
-    Title = "Job Truk",
-    Default = false,
-    Callback = function(state)
-        -- Kode teleportasi Anda di sini
-    end
-})
+    -- Menunggu sampai vehicle mencapai tujuan
+    Vehicle.PrimaryPart.Anchored = true
+    wait(2) -- Menunggu selama 2 detik (sesuaikan dengan durasi yang diinginkan)
+    Vehicle.PrimaryPart.Anchored = false
 
--- Anti AFK
-Other:AddToggle("Anti AFK", {
-    Title = "Anti AFK",
-    Default = false,
-    Callback = function(state)
-        -- Kode Anti AFK Anda di sini
-    end
-})
+    Character.Parent = workspace
+end
 
--- Hapus Map
-Other:AddButton("Hapus Map", {
-    Title = "Hapus Map",
-    Callback = function()
-        -- Kode Hapus Map Anda di sini
-    end
-})
+local function GetWaypointDestination(waypoint)
+    local destinations = {
+        ["Silambat Palimanan"] = Vector3.new(-13001.578, 1058.455, -16360.201),
+        ["Rest Area Km 279"] = Vector3.new(-25910.939, 1059.604, -43896.273),
+        ["SuperMart Pekalongan"] = Vector3.new(-38720.934, 1018.218, -62491.059),
+        ["PT.CDID Cargo Cirebon"] = Vector3.new(-21803.887, 1046.989, -27817.059),
+        ["Rojod Semarang"] = Vector3.new(-50889.66, 1017.867, -86514.797)
+    }
 
--- Hapus Trailer
-Other:AddButton("Hapus Trailer", {
-    Title = "Hapus Trailer",
-    Callback = function()
-        -- Kode Hapus Trailer Anda di sini
-    end
-})
+    return destinations[waypoint]
+end
 
--- Dealer
-local dealerContainer = game:GetService("Players").LocalPlayer.PlayerGui.Dealership.Container.Dealership.Dealerlist
-for _, dealer in ipairs(dealerContainer:GetChildren()) do
-    local carNamesAndPrices = {}
-    for _, car in ipairs(dealer:GetChildren()) do
-        if car and car:FindFirstChild("Frame") and car.Frame:FindFirstChild("CarName") and car.Frame:FindFirstChild("Type") and car.Frame.Type:FindFirstChild("New") and car.Frame.Type.New.Visible and car.Frame:FindFirstChild("Price") then
-            local carName = car.Frame.CarName.Text
-            local carPrice = car.Frame.Price.Text
-            table.insert(carNamesAndPrices, carName .. " (" .. carPrice .. ")")
+do
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Auto Job Truck", Default = false })
+
+    Toggle:OnChanged(function()
+        --code autojob
+    end)
+
+    Options.MyToggle:SetValue(false)
+
+
+    
+    local Slider = Tabs.Main:AddSlider("Slider", {
+        Title = "Walkspeed",
+        Description = "This is a Walkspeed",
+        Default = 16,
+        Min = 16,
+        Max = 500,
+        Rounding = 1,
+        Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
         end
-    end
-    Other:AddDropdown(dealer.Name, {
-        Title = dealer.Name,
-        Values = carNamesAndPrices,
+    })
+
+    Slider:OnChanged(function(Value)
+        print("Slider changed:", Value)
+    end)
+
+    Slider:SetValue(16)
+
+
+
+    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+        Title = "Dropdown",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
         Multi = false,
         Default = 1,
     })
+
+    Dropdown:SetValue("four")
+
+    Dropdown:OnChanged(function(Value)
+        print("Dropdown changed:", Value)
+    end)
+
+
+    local Keybind = Tabs.Main:AddKeybind("Keybind", {
+        Title = "KeyBind",
+        Mode = "Toggle", -- Always, Toggle, Hold
+        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+        -- Occurs when the keybind is clicked, Value is `true`/`false`
+        Callback = function(Value)
+            print("Keybind clicked!", Value)
+        end,
+
+        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+        ChangedCallback = function(New)
+            print("Keybind changed!", New)
+        end
+    })
+
+    -- OnClick is only fired when you press the keybind and the mode is Toggle
+    -- Otherwise, you will have to use Keybind:GetState()
+    Keybind:OnClick(function()
+        print("Keybind clicked:", Keybind:GetState())
+    end)
+
+    Keybind:OnChanged(function()
+        print("Keybind changed:", Keybind.Value)
+    end)
+
+    task.spawn(function()
+        while true do
+            wait(1)
+
+            -- example for checking if a keybind is being pressed
+            local state = Keybind:GetState()
+            if state then
+                print("Keybind is being held down")
+            end
+
+            if Fluent.Unloaded then break end
+        end
+    end)
+
+    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+
+
+    local Input = Tabs.Main:AddInput("Input", {
+        Title = "Input",
+        Default = "Default",
+        Placeholder = "Placeholder",
+        Numeric = false, -- Only allows numbers
+        Finished = false, -- Only calls callback when you press enter
+        Callback = function(Value)
+            print("Input changed:", Value)
+        end
+    })
+
+    Input:OnChanged(function()
+        print("Input updated:", Input.Value)
+    end)
 end
 
--- Inisialisasi Fluent
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
 Fluent:Notify({
-    Title = "Fluent",
-    Content = "The script has been loaded.",
+    Title = "KWEK",
+    Content = "Udah Aktif Bos.",
     Duration = 8
 })
+
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
 SaveManager:LoadAutoloadConfig()
