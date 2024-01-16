0f2b94102d29b675d54b8eabebe3518a6c4732de
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2024-01-16 11:00:51 +0700

    Update bebekfluent.lua

--- bebekfluent.lua
+++ bebekfluent.lua
@@ -1,177 +1,216 @@
-local teleportEnabled = false
-local teleportTimer = 48
-
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Bebek Hub v1.2.1 Fluent 9",
+    Title = "Fluent " .. Fluent.Version,
     SubTitle = "by dawid",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
-    Acrylic = true,
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
     Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
-local Main = Window:AddTab({ Title = "Main", Icon = "" })
-local Truck = Window:AddTab({ Title = "Truck", Icon = "" })
-local Other = Window:AddTab({ Title = "Other", Icon = "" })
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
 
--- Main Tab
-Main:AddSlider("Walkspeed", {
-    Title = "Walkspeed",
-    Description = "Adjust your character's walk speed.",
-    Default = 16,
-    Min = 16,
-    Max = 500,
-    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-    end
-})
+local Options = Fluent.Options
 
-Main:AddSlider("Jumppower", {
-    Title = "Jumppower",
-    Description = "Adjust your character's jump power.",
-    Default = 50,
-    Min = 50,
-    Max = 350,
-    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-    end
-})
+--cet
+local teleportEnabled = false
+local teleportTimer = 48
+--end cet
 
--- Truck Tab
--- Your teleport functions here...
+--tp
+local function TeleportToDestination(destination)
+    -- Your teleport function here
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local Character = LocalPlayer.Character
+    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
 
-Truck:AddButton({
-    Title = "Silambat Palimanan",
-    Callback = function()
-        TeleportToPalimanan()
-    end
-})
+    local SeatPart = Humanoid.SeatPart
+    local Vehicle = SeatPart.Parent
 
-Truck:AddButton({
-    Title = "PT.CDID Cargo Cirebon",
-    Callback = function()
-        TeleportToCirebon()
-    end
-})
-
-Truck:AddButton({
-    Title = "Rest Area Km 279",
-    Callback = function()
-        TeleportToKM279()
-    end
-})
-
-Truck:AddButton({
-    Title = "SuperMart Pekalongan",
-    Callback = function()
-        TeleportToPekalongan()
-    end
-})
-
-Truck:AddButton({
-    Title = "Rojod Semarang",
-    Callback = function()
-        TeleportToSemarang()
-    end
-})
-
--- Other Tab
-Other:AddToggle("Job Truk", {
-    Title = "Job Truk",
-    Default = false,
-    Callback = function(state)
-        -- Your Job Truk code here...
-    end
-})
-
-Other:AddToggle("Anti AFK", {
-    Title = "Anti AFK",
-    Default = false,
-    Callback = function(state)
-        -- Your Anti AFK code here...
-    end
-})
-
-Other:AddToggle("Auto Truck Trip", {
-    Title = "Auto Truck Trip",
-    Default = false,
-    Callback = function(state)
-        -- Your Auto Truck Trip code here...
-    end
-})
-
-Other:AddButton({
-    Title = "Hapus Map",
-    Callback = function()
-        -- Your Hapus Map code here...
-    end
-})
-
-Other:AddButton({
-    Title = "Hapus Trailer",
-    Callback = function()
-        -- Your Hapus Trailer code here...
-    end
-})
-
--- Spawn function
-spawn(function()
-    while true do
-        if teleportEnabled then
-            if teleportTimer <= 0 then
-                local waypointFolder = game:GetService("Workspace").Etc.Waypoint
-                if waypointFolder then
-                    local waypoint = waypointFolder.Waypoint
-                    if waypoint then
-                        local textLabel = waypoint.BillboardGui.TextLabel
-
-                        local waypointDestination = GetWaypointDestination(textLabel.Text)
-                        if waypointDestination then
-                            TeleportToDestination(waypointDestination)
-                        end
-                    end
-                end
-
-                teleportTimer = 48
-            else
-                teleportTimer = teleportTimer - 1
-            end
-        end
-        wait(1)
-    end
-end)
-
--- Dealer code
-local dealerContainer = game:GetService("Players").LocalPlayer.PlayerGui.Dealership.Container.Dealership.Dealerlist
-local CarSection = Window:AddTab({ Title = "Cars", Icon = "" })
-
-for _, dealer in ipairs(dealerContainer:GetChildren()) do
-    -- Membuat daftar nama mobil dan harga untuk dealer ini
-    local carNamesAndPrices = {}
-    for _, car in ipairs(dealer:GetChildren()) do
-        if car and car:FindFirstChild("Frame") and car.Frame:FindFirstChild("CarName") and car.Frame:FindFirstChild("Type") and car.Frame.Type:FindFirstChild("New") and car.Frame.Type.New.Visible and car.Frame:FindFirstChild("Price") then
-            local carName = car.Frame.CarName.Text
-            local carPrice = car.Frame.Price.Text
-            table.insert(carNamesAndPrices, carName .. " (" .. carPrice .. ")")
-        end
+    if SeatPart:FindFirstAncestor("Body") then
+        Vehicle = SeatPart:FindFirstAncestor("Body").Parent
     end
 
-    -- Membuat dropdown dengan nama mobil dan harga untuk dealer ini
-    CarSection:AddDropdown({
-        Title = dealer.Name,
-        Values = carNamesAndPrices,
-        Multi = false,
-        Default = 1,
-        Callback = function(Value)
-            -- Anda bisa menambahkan fungsi yang diinginkan di sini
-        end
-    })
+    Vehicle.PrimaryPart = SeatPart
+
+    Character.Parent = Vehicle
+    Vehicle:MoveTo(destination)
+
+    -- Menunggu sampai vehicle mencapai tujuan
+    Vehicle.PrimaryPart.Anchored = true
+    wait(2) -- Menunggu selama 2 detik (sesuaikan dengan durasi yang diinginkan)
+    Vehicle.PrimaryPart.Anchored = false
+
+    Character.Parent = workspace
 end
 
--- Call the Fluent Library's Init function to finish setting up the UI
-Fluent:Init()
+local function GetWaypointDestination(waypoint)
+    local destinations = {
+        ["Silambat Palimanan"] = Vector3.new(-13001.578, 1058.455, -16360.201),
+        ["Rest Area Km 279"] = Vector3.new(-25910.939, 1059.604, -43896.273),
+        ["SuperMart Pekalongan"] = Vector3.new(-38720.934, 1018.218, -62491.059),
+        ["PT.CDID Cargo Cirebon"] = Vector3.new(-21803.887, 1046.989, -27817.059),
+        ["Rojod Semarang"] = Vector3.new(-50889.66, 1017.867, -86514.797)
+    }
+
+    return destinations[waypoint]
+end
+--end tp
+
+do
+    Fluent:Notify({
+        Title = "Aktif Bos",
+        Content = "BUBUUUBUBUBU",
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
+
+
+
+    -- Tabs.Main:AddParagraph({
+    --     Title = "Paragraph",
+    --     Content = "This is a paragraph.\nSecond line!"
+    -- })
+
+
+
+    Tabs.Main:AddButton({
+        Title = "Button",
+        Description = "Very important button",
+        Callback = function()
+            Window:Dialog({
+                Title = "Title",
+                Content = "This is a dialog",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            print("Confirmed the dialog.")
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                            print("Cancelled the dialog.")
+                        end
+                    }
+                }
+            })
+        end
+    })
+
+
+    --toggle auto job truck
+    local Toggle = Tabs.Main:AddToggle("Auto Job Truck", {Title = "Toggle", Default = false })
+
+    Toggle:OnChanged(function()
+        --print("Auto Job Truck Start:", Options.MyToggle.Value)
+        Fluent:Notify({
+            Title = "Notification",
+            Content = "Auto Job Truck Start",
+            SubContent = "SubContent", -- Optional
+            Duration = 3 -- Set to nil to make the notification not disappear
+        })
+    end)
+
+    Options.MyToggle:SetValue(false)
+    -- end toggle auto job truck
+
+    --Speed
+    local Slider = Tabs.Main:AddSlider("Slider", {
+        Title = "Slider",
+        Description = "This is a slider",
+        Default = 20,
+        Min = 20,
+        Max = 500,
+        Rounding = 1,
+        Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+        end
+    })
+
+    Slider:OnChanged(function(Value)
+        --print("Slider changed:", Value)
+    end)
+
+    Slider:SetValue(3)
+    --END speed
+
+
+    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+        Title = "Dropdown",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = false,
+        Default = 1,
+    })
+
+    Dropdown:SetValue("one")
+
+    Dropdown:OnChanged(function(Value)
+        print("Dropdown changed:", Value)
+    end)
+
+
+
+
+    local Input = Tabs.Main:AddInput("Input", {
+        Title = "Input",
+        Default = "Default",
+        Placeholder = "Placeholder",
+        Numeric = false, -- Only allows numbers
+        Finished = false, -- Only calls callback when you press enter
+        Callback = function(Value)
+            print("Input changed:", Value)
+        end
+    })
+
+    Input:OnChanged(function()
+        print("Input updated:", Input.Value)
+    end)
+end
+
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
+})
+
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
