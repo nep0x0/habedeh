e360bc8692f7c740d4af8dfd5835ee56d9569b1e
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2024-01-16 13:15:10 +0700

    Update bebekfluent.lua

--- bebekfluent.lua
+++ bebekfluent.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Kohvceng " .. Fluent.Version,
+    Title = "Kohvcengas " .. Fluent.Version,
     SubTitle = "by dawid",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
